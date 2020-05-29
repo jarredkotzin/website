622a9566bd90859ec4edd258c7c52981b81321a6
Jarred Kotzin <59810486+jarredkotzin@users.noreply.github.com>
2020-05-28 21:24:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('keyup', spacebar);
+
+function spacebar(e) {
+  if(e.keyCode == 32) {
+    console.log("space clicked");
+    document.body.style.backgroundColor = "orange"; 
+  }
+  console.log(e.keyCode);
+}
